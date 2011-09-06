77cf39a6bc30b760fd7f07adda21479687f0796d
toby <toby.hster@hotmail.com>
2011-09-06 16:19:16 +1200

    10004 done

--- 10004/main.cpp
+++ 10004/main.cpp
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <string>
 #include <vector>
+#include <stack>
 
 #define DEBUG 0
 
@@ -16,5 +17,61 @@ void debug(string print){
 }
 
 int main(void){
+   int n, m;
+   while(true){
+      cin >> n;
+      if(n == 0){
+         break;
+      } else{
+         bool illegal = false;
+         cin >> m;
+         vector< vector<bool> > graph(n,vector<bool>(n));
+         for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+               graph[i][j] = false;
+            }
+         }
+         for( int i = 0; i < m; i++){
+            int x, y;
+            cin >> x >> y;
+            graph[x][y] = true;
+            graph[y][x] = true;
+         }
+         vector<int> visited(n);
+         for(int i = 0; i < n; i++){
+            // 0 = unvisted, 1= white, 2 = red
+            visited[i] = 0;
+         }
+         stack<int> fringe;
+         fringe.push(0);
+         //0= unvisited, 1= red, 2= white
+         visited[0] = 1;
+         while(!fringe.empty() && !illegal){
+            int curr = fringe.top();
+            fringe.pop();
+            int colour = visited[curr];
+            for(int i = 0; i < n; i++){
+               if(graph[curr][i]){
+                  if(visited[i] == 0){
+                     if(colour == 1){
+                        visited[i] = 2;
+                     } else{
+                        visited[i] = 1;
+                     }
+                     fringe.push(i);
+                  } else if(visited[i] == colour){
+                     illegal = true;
+                     break;
+                  }
+               }
+            }
+         }
+         if(illegal){
+            cout << "NOT BICOLORABLE." << endl;
+         } else{
+            cout << "BICOLORABLE." << endl;
+         }
+      }
+   }
    return 0;
 }
